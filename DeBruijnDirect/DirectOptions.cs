f0e03ad1cb4e62ee474064d81f29203f8cd6ad4b
agmarchuk <mag@iis.nsk.su>
2020-02-18 15:38:34 +0700

    Варианты конфигурации

--- DeBruijnDirect/DirectOptions.cs
+++ DeBruijnDirect/DirectOptions.cs
@@ -6,7 +6,7 @@ namespace DeBruijnDirect
 {
     class DirectOptions
     {
-        public static string readsfilename = @"D:\Home\data\deBruijn\reads.txt";
+        public static string readsfilename = @"D:\Home\data\deBruijn\Gen_reads.txt";
         //public static string readsfilename = @"C:\data\deBruijn\Gen_reads.txt";
         public static string bytereadsfilename = @"D:\Home\data\deBruijn\bytereads.txt";
         public static string workdir = @"D:\Home\data\deBruijn\";
