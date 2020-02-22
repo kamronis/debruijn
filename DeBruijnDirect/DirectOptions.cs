9edff6dff7879a7cb88d6d9adaac84edcc0e9c5d
agmarchuk <mag@iis.nsk.su>
2020-02-22 13:23:08 +0700

    Сделал (бинарный) генератор ReadsGen

--- DeBruijnDirect/DirectOptions.cs
+++ DeBruijnDirect/DirectOptions.cs
@@ -9,10 +9,11 @@ namespace DeBruijnDirect
         //public static string readsfilename = @"D:\Home\data\deBruijn\reads.txt";
         public static string readsfilename = @"D:\Home\data\deBruijn\Gen_reads.txt";
         public static string bytereadsfilename = @"D:\Home\data\deBruijn\bytereads.txt";
-        public static string compressedreadsfilename = @"D:\Home\data\deBruijn\bytereads_compressed.bin";
+        public static string compressedreadsfilename = @"D:\Home\data\deBruijn\G_reads.bin";
+        //public static string compressedreadsfilename = @"D:\Home\data\deBruijn\bytereads_compressed.bin";
         public static string workdir = @"D:\Home\data\deBruijn\";
 
-        public static int nsymbols = 20;
+        public static int nsymbols = 31;
         public static int npasses = 1;
         public static int nsections = 4;
 
